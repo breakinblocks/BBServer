ae5a018f30461e62cc282ce367cc6bb501e9cb22
BlueAgent <BlueAgent@users.noreply.github.com>
2018-06-12 23:22:19 +0800

    Added notifications for 2 hours and 1 hour till restart

--- src/main/java/com/breakinblocks/bbserver/module/Restart.java
+++ src/main/java/com/breakinblocks/bbserver/module/Restart.java
@@ -15,8 +15,8 @@ import java.io.IOException;
 
 @Mod.EventBusSubscriber(modid = BBServer.MODID)
 public class Restart {
-    // 30 minutes, 10 minutes, 5 minutes, 1 minutes, 30 seconds, 10 seconds, 5, 4, 3, 2, 1
-    static final long[] NOTIFICATIONS = new long[]{30 * 60, 10 * 60, 5 * 60, 60, 30, 10, 5, 4, 3, 2, 1};
+    // 2 hours, 1 hour, 30 minutes, 10 minutes, 5 minutes, 1 minutes, 30 seconds, 10 seconds, 5, 4, 3, 2, 1
+    static final long[] NOTIFICATIONS = new long[]{2 * 60 * 60, 60 * 60, 30 * 60, 10 * 60, 5 * 60, 60, 30, 10, 5, 4, 3, 2, 1};
     public static boolean restarting = false;
     static long restartTime = -1;
     static int nextRestartMessage = 0;
@@ -26,10 +26,11 @@ public class Restart {
         if (event.phase != TickEvent.Phase.END) return;
         if (restarting) return;
         if (Config.Restart.period > 0) {
-            // Restart Initialisation
+            // Initialisation
             if (restartTime == -1) {
                 restartTime = System.currentTimeMillis() + (long) (Config.Restart.period * 1000 * 60 * 60);
                 long remaining = (restartTime - System.currentTimeMillis()) / 1000;
+                // Select closest notification
                 while (nextRestartMessage < NOTIFICATIONS.length && remaining <= NOTIFICATIONS[nextRestartMessage]) {
                     nextRestartMessage++;
                 }
@@ -37,26 +38,32 @@ public class Restart {
 
             // Seconds remaining
             long remaining = (restartTime - System.currentTimeMillis()) / 1000;
-            if (remaining > 0) {
-                if (nextRestartMessage < NOTIFICATIONS.length && remaining <= NOTIFICATIONS[nextRestartMessage]) {
-                    while (nextRestartMessage < NOTIFICATIONS.length && remaining <= NOTIFICATIONS[nextRestartMessage]) {
-                        nextRestartMessage++;
-                    }
-                    remaining = NOTIFICATIONS[nextRestartMessage - 1];
-                    String message;
-                    if (remaining >= 60) {
-                        message = (remaining / 60) + " min";
-                    } else {
-                        message = remaining + " sec";
-                    }
-                    message = "Restart in " + message;
-                    FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().sendMessage(
-                            ChatUtils.coloredString(message, TextFormatting.LIGHT_PURPLE),
-                            true
-                    );
+
+            // Restart Notifications
+            if (remaining > 0 && nextRestartMessage < NOTIFICATIONS.length && remaining <= NOTIFICATIONS[nextRestartMessage]) {
+                // Select next closest notification
+                while (nextRestartMessage < NOTIFICATIONS.length && remaining <= NOTIFICATIONS[nextRestartMessage]) {
+                    nextRestartMessage++;
                 }
+                // Seconds for current notification
+                remaining = NOTIFICATIONS[nextRestartMessage - 1];
+
+                String message;
+                if (remaining >= 60 * 60) {
+                    message = (remaining / 60 / 60) + " hr";
+                }else if (remaining >= 60) {
+                    message = (remaining / 60) + " min";
+                } else {
+                    message = remaining + " sec";
+                }
+                message = "Restart in " + message;
+                FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().sendMessage(
+                        ChatUtils.coloredString(message, TextFormatting.LIGHT_PURPLE),
+                        true
+                );
             }
 
+            // Restart
             if (System.currentTimeMillis() > restartTime) {
                 try {
                     restart();
