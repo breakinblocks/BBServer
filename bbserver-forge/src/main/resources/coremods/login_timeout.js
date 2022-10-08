8358fdf8fd6ebd94f623d65882355bfc041f6c5e
BlueAgent <BlueAgent@users.noreply.github.com>
2022-10-08 17:42:34 +0800

    Clean up and fix the login timeout coremod

--- bbserver-forge/src/main/resources/coremods/login_timeout.js
+++ bbserver-forge/src/main/resources/coremods/login_timeout.js
@@ -13,28 +13,31 @@ function initializeCoreMod() {
                 var Opcodes = Java.type('org.objectweb.asm.Opcodes');
                 var IntInsnNode = Java.type('org.objectweb.asm.tree.IntInsnNode');
 
-                var timeoutTicks = ASMAPI.findFirstInstruction(method, Opcodes.SIPUSH);
-
-                while (true) {
-                    if (timeoutTicks == null) {
-                        ASMAPI.log('WARN', '[login_timout] Failed find the SIPUSH instruction with 600 ticks.');
-                        return method;
-                    }
-
-                    if (timeoutTicks instanceof IntInsnNode && timeoutTicks.operand === 600) {
-                        break;
-                    }
-
-                    timeoutTicks = ASMAPI.findFirstInstruction(method, Opcodes.SIPUSH, method.instructions.indexOf(timeoutTicks));
-                }
+                ASMAPI.log('INFO', '[login_timout] Patching...');
 
                 var hook = ASMAPI.buildMethodCall('com/breakinblocks/bbserver/coremods/LoginTimeoutCoremod',
                     'modifyLoginTimeout',
                     '(I)I',
                     ASMAPI.MethodType.STATIC);
-                method.instructions.insert(timeoutTicks, hook);
 
-                ASMAPI.log('INFO', '[login_timout] Patched.');
+                var patched = 0;
+                var timeoutTicks = ASMAPI.findFirstInstruction(method, Opcodes.SIPUSH);
+
+                while (timeoutTicks != null) {
+                    if (timeoutTicks instanceof IntInsnNode && timeoutTicks.operand === 600) {
+                        method.instructions.insert(timeoutTicks, hook.clone({}));
+                        patched++;
+                    }
+
+                    timeoutTicks = ASMAPI.findFirstInstructionAfter(method, Opcodes.SIPUSH, 1 + method.instructions.indexOf(timeoutTicks));
+                }
+
+                if (!patched) {
+                    ASMAPI.log('WARN', '[login_timout] Failed find any SIPUSH instructions with operand 600.');
+                } else {
+                    ASMAPI.log('INFO', '[login_timout] Patched ' + patched + ' SIPUSH instruction(s) with operand 600.');
+                }
+
                 return method;
             }
         }
